0c504a57ef250d3f5b4a2a1c9237a4598cfd9087
glimmerald <glimmerald@gmail.com>
2024-06-12 22:13:56 +0300

    qwe

---
+++ src/components/prototip.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="container">
+    <div class="do-you-like-section">
+        <h2>Всё понравилось?</h2>
+        <div class="stars">
+            
+        </div>
+    </div>
+</div>
+</template>
+
+<script setup>
+
+
+</script>
